032023114b8a2b6a5b1278aae6cc2b2682a75dfd
Kraiba Semakula <krabz01@gmail.com>
2018-09-05 22:10:57 +0300

    complete table

--- src/ScheduleTable.tsx
+++ src/ScheduleTable.tsx
@@ -8,34 +8,37 @@ export interface IProps {
 }
 export default class ScheduleTable extends React.Component<IProps, any> {
   public render() {
-    const products = [
-      {
-        id: 1,
-        name: "Product1",
-        price: 120
-      },
-      {
-        id: 2,
-        name: "Product2",
-        price: 80
-      }
-    ];
     const options = {
       exportCSVText: "Export"
     };
+
     return (
       <BootstrapTable
-        data={products}
+        data={this.props.schedules}
         options={options}
         striped={true}
         hover={true}
         exportCSV={true}
       >
-        <TableHeaderColumn isKey={true} dataField="id">
-          Product ID
+        <TableHeaderColumn isKey={true} dataField="teammateName">
+          Teammate
         </TableHeaderColumn>
-        <TableHeaderColumn dataField="name">Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+        <TableHeaderColumn dataField="teammateType">
+          Employee Type
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="monday">Monday 2/12</TableHeaderColumn>
+        <TableHeaderColumn dataField="tuesday">Tuesday 2/13</TableHeaderColumn>
+        <TableHeaderColumn dataField="wednesday">
+          Wednesday 2/14
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="thursday">
+          Thursday 2/15
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="friday">Friday 2/16</TableHeaderColumn>
+        <TableHeaderColumn dataField="saturday">
+          Saturday 2/18
+        </TableHeaderColumn>
+        <TableHeaderColumn dataField="sunday">Sunday 2/17</TableHeaderColumn>
       </BootstrapTable>
     );
   }
